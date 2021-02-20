99324426ef77733405b23bcc1f8510f7a84e7fed
KilJaeeun <rha3122@naver.com>
2021-02-20 10:06:18 +0900

    초기환경 세팅

---
+++ src/main/java/com/giftclub/GiftclubApplication.java
@@ -0,0 +1,13 @@
+package com.giftclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftclubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiftclubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/giftclub/GiftclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giftclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftclubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
