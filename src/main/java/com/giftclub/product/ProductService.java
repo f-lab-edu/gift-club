ac251d900a6dc4b965b560cc929e84e93bd8777c
rudus1012 <rudus1012@naver.com>
2021-04-27 03:20:19 +0900

    feat: 🎸 상품 수정하기 기능 구현
    - 상점 주인만 상품을 수정할 수 있도록 기능 구현
    
    ✅ Closes: #27

--- src/main/java/com/giftclub/product/ProductService.java
+++ src/main/java/com/giftclub/product/ProductService.java
@@ -1,6 +1,11 @@
 package com.giftclub.product;
 
+import com.giftclub.common.SessionUtils;
+import com.giftclub.common.exception.UserNotFoundException;
+import com.giftclub.common.exception.ValidationException;
 import com.giftclub.mapper.ProductMapper;
+import com.giftclub.mapper.StoreMapper;
+import com.giftclub.store.Store;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class ProductService {
 
     private final ProductMapper productMapper;
+    private final StoreMapper storeMapper;
+    private final SessionUtils sessionUtils;
 
     public void insertProduct(Product product) {
         productMapper.insertProduct(product);
@@ -26,5 +33,17 @@ public class ProductService {
         Product productStock = selectProductById(id);
 
         productMapper.decreaseProduct(id, productStock.getStock() - 1);
+    }
+
+    @Transactional
+    public void updateProduct(Product product) {
+
+        Store store = storeMapper.selectStoreById(product.getStoreId());
+
+        if (store.getMemberId().equals(sessionUtils.getLoginMember().getMemberId())) {
+            productMapper.updateProduct(product);
+        } else {
+            throw new UserNotFoundException("상점 주인의 권한이 없습니다.");
+        }
     }
 }
